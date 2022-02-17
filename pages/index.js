5ba8f5b298710a4318250db853b634e116c10066
seanmodd <sean@vestaridefair.com>
2022-02-17 15:55:46 -0800

    1 file modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
+import MyComponent from '../components/MyComponent';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <Layout>
-      <h3>Documentation &rarr;</h3>
+      <MyComponent />
     </Layout>
   );
 }
