578489d84278ed7bad59eef5b85330844e797e76
seanmodd <sean@vestaridefair.com>
2022-02-17 15:59:36 -0800

    1 file modified

--- components/MyCounter.js
+++ components/MyCounter.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
 
 const MyCounter = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h3>MyCounter is this</h3>
+      <p>Count is: {count}</p>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <button
+          style={{
+            margin: '15px',
+          }}
+        >
+          Increase Count
+        </button>
+        <button
+          style={{
+            margin: '15px',
+          }}
+        >
+          Decrease Count
+        </button>
+      </div>
     </div>
   );
 };
