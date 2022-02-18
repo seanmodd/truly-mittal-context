f2b57e0d1097ba3c0fe695e0b82571211cde0e02
seanmodd <sean@vestaridefair.com>
2022-02-17 16:05:22 -0800

    template it

--- components/MyCounter.js
+++ components/MyCounter.js
@@ -2,13 +2,19 @@ import { useState } from 'react';
 
 const MyCounter = () => {
   const [count, setCount] = useState(0);
+  const increaseCount = (params) => {
+    console.log('increase count pushed');
+  };
+  const decreaseCount = (params) => {
+    console.log('increase count pushed');
+  };
   return (
     <div>
       <h3>MyCounter is this</h3>
       <p>Count is: {count}</p>
 
-      <button>Increase Count</button>
-      <button>Decrease Count</button>
+      <button onClick={increaseCount}>Increase Count</button>
+      <button onClick={decreaseCount}>Decrease Count</button>
     </div>
   );
 };
