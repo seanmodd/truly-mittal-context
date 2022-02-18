262538c441cb163211da70e0603bdd926db2749b
seanmodd <sean@vestaridefair.com>
2022-02-17 16:08:12 -0800

    1 file modified

--- components/MyCounter.js
+++ components/MyCounter.js
@@ -3,10 +3,11 @@ import { useState } from 'react';
 const MyCounter = () => {
   const [count, setCount] = useState(0);
   const increaseCount = (params) => {
-    console.log('increase count pushed');
+    setCount(count + 1);
   };
   const decreaseCount = (params) => {
     console.log('increase count pushed');
+    setCount(count - 1);
   };
   return (
     <div>
